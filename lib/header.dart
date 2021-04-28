dc9cc21efc2b1a016cedc5bb50749eb68d0a4fab
Andrii Matviichuk <matviichuk.andr@gmail.com>
2021-04-28 17:53:10 +0200

    Added user info

--- lib/header.dart
+++ lib/header.dart
@@ -13,31 +13,70 @@ class _HeaderState extends State<Header> {
     Size screenSize = MediaQuery.of(context).size;
     return SliverList(
       delegate: SliverChildListDelegate([
-        Column(
+        Stack(
           children: [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              height: screenSize.height / 5,
-              decoration: BoxDecoration(
-                  color: Colors.teal,
-                  borderRadius: BorderRadius.vertical(
-                    bottom: Radius.circular(45),
-                  ),
-                  boxShadow: [BoxShadow(blurRadius: 3)]),
-              child: Column(
-                children: [
-                  SizedBox(height: 20),
-                  Row(
+            Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  height: screenSize.height / 5,
+                  decoration: BoxDecoration(
+                      color: Colors.teal,
+                      borderRadius: BorderRadius.vertical(
+                        bottom: Radius.circular(45),
+                      ),
+                      boxShadow: [BoxShadow(blurRadius: 3)]),
+                  child: Column(
                     children: [
-                      CircleAvatar(
-                        backgroundColor: Colors.white70,
-                        radius: 35,
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.white70,
+                            radius: 35,
+                            child: CircleAvatar(
+                                backgroundImage:
+                                    AssetImage("images/avatar.png"),
+                                radius: 30),
+                          ),
+                          SizedBox(width: 10),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Lisa Snow",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  )),
+                              SizedBox(height: 3),
+                              Container(
+                                padding: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(25),
+                                  color: Colors.yellow.shade200,
+                                ),
+                                child: Text("Premium Member",
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.black,
+                                    )),
+                              )
+                            ],
+                          ),
+                          Spacer(),
+                          Text("227 \$ USD",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white))
+                        ],
                       )
                     ],
-                  )
-                ],
-              ),
-            )
+                  ),
+                )
+              ],
+            ),
           ],
         )
       ]),
